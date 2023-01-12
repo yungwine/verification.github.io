fbf2c52d4b819e46c41504ce9670447d28979aae
yungwine <94808996+yungwine@users.noreply.github.com>
2023-01-12 18:04:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
 
-function() {
-    function str_rand() {
-        var result       = '';
-        var words        = '0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
-        var max_position = words.length - 1;
-            for( i = 0; i < 5; ++i ) {
-                position = Math.floor ( Math.random() * max_position );
-                result = result + words.substring(position, position + 1);
-            }
-        return result;
-    }
+//function() {
+//    function str_rand() {
+//        var result       = '';
+//        var words        = '0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+//        var max_position = words.length - 1;
+//            for( i = 0; i < 5; ++i ) {
+//                position = Math.floor ( Math.random() * max_position );
+//                result = result + words.substring(position, position + 1);
+//            }
+//        return result;
+//    }
 
 let item = "";
 
